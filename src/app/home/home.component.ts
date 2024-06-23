48257c691c40a0e79c79d8d03d121dc671554d14
Poojaputtaswamy <poojaputtaswamy07@gmail.com>
2024-06-23 11:29:56 +0200

    create and update home-page

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {
+  constructor(private http: HttpClient, private router: Router) {}
+
+  logout() {
+    this.router.navigate(['/login']);
+  }
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+}
