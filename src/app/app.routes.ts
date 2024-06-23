c16d47ba41e2f8f19cd2eb4f1d029ea32a255f3d
Poojaputtaswamy <poojaputtaswamy07@gmail.com>
2024-06-23 11:31:21 +0200

    update in settings and configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LogicComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 export const routes: Routes = [
-  {
-    path: 'register',
-    component: RegisterComponent,
-  },
-  {
-    path: 'login',
-    component: LogicComponent,
-  },
+  { path: 'login', component: LogicComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
+@NgModule({
+  imports: [RouterModule.forRoot(routes), FormsModule],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
